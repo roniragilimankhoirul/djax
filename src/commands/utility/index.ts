8c909c1310756b0c10f1c77650bd32d0c6a02439
roniragilimankhoirul <ronisensei7@gmail.com>
2024-03-19 00:04:31 +0700

    feat: Add salam command

--- src/commands/utility/index.ts
+++ src/commands/utility/index.ts
@@ -1,5 +1,7 @@
 import * as ping from "./pingCommand";
+import * as salam from "./salamCommand";
 
 export const commands = {
   ping,
+  salam,
 };
--- test/index.test.ts
+++
@@ -1,5 +0,0 @@
-import { it, describe } from "bun:test";
-
-describe("Name of the group", () => {
-  it("should ", () => {});
-});
